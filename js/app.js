1eb263e577b66ebaa5168233e57d00abc11c18e2
powerfist01 <sujeets207@gmail.com>
2020-04-12 20:42:15 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('metreInput');
+const output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
+
+input.addEventListener('input', (e) => {
+  let metre = e.target.value;
+
+  output.style.visibility = 'visible';
+
+  let centi = document.getElementById('centiOutput');
+  centi.innerHTML = metre * 100;
+
+  let kilo = document.getElementById('kiloOutput');
+  kilo.innerHTML = metre/1000;
+
+  let inch = document.getElementById('inchOutput');
+  inch.innerHTML = metre * 39.37;
+
+  e.preventDefault();
+});
